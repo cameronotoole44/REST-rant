eac02d0b03c3ca049277549e1db5129dd75dcf5f
cameronotoole44 <cameronotoole404@gmail.com>
2024-04-20 22:24:30 -0400

    added other jsx files

---
+++ views/places/edit.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+const Def = require('../default');
+
+function editForm(data) {
+    return (
+        <Def>
+            <main>
+                <h1>EDIT</h1>
+            </main>
+        </Def>
+    )
+}
---
+++ views/places/new.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+const Def = require('../default');
+
+function newForm(data) {
+    return (
+        <Def>
+            <main>
+                <h1>ENW FORM</h1>
+            </main>
+        </Def>
+    )
+}
