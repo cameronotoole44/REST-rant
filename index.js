ba19f20fc397c7ff8b6b1ddfa41d7e2117a42431
cameronotoole44 <cameronotoole404@gmail.com>
2024-04-20 22:24:06 -0400

    added places-mock data

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ const app = express();
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
+
+
 app.use('/places', require('./controllers/places'));
 
 app.get('/', (req, res) => {
     res.render('home');
 })
-
-// app.get('*', (req, res) => {
-//     res.status(404).send('<h1>404 Not Found, this page is currently sleeping</h1>');
-// })
 
 app.get('*', (req, res) => {
     res.render('error404');
