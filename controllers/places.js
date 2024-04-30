e5a9bec335e825e84c0b6f4b28f51d74747e9f99
cameronotoole44 <cameronotoole404@gmail.com>
2024-04-30 14:44:09 -0400

    mongoose ODM

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
-const places = require('../models/places.js');
+const db = require('../models');
 
-// HOME-INDEX //
+// INDEX //
 router.get('/', (req, res) => {
+    db.Place.find()
+        .then((places) => {
+            res.render('places/index', { places })
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('error404')
+        })
+});
 
-    res.render('places/index', { places })
+// CREATE // 
+router.post('/', (req, res) => {
+    db.Place.create(req.body)
+        .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 });
 
 // NEW //
@@ -14,89 +32,150 @@ router.get('/new', (req, res) => {
 
 // SHOW //
 router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        res.render('places/show', { place: places[id], id: id })
-    }
+    db.Place.findById(req.params.id)
+        .then(place => {
+            res.render('places/show', { place })
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 });
 
-// EDIT // PASSED CHECK
-router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        res.render('places/edit', { place: places[id], id: id })
-    }
-});
-
-// UPDATE // LANDS AT UNDEFINED?_METHOD=PUT ????
+// UPDATE // 
 router.put('/:id', (req, res) => {
-    let id = Number(req.params.id); // i don't like this!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    console.log('id:', id); // returns NaN ?????????????
-    console.log('places:', places);
-    if (isNaN(id)) {  // if places[id] is falsy - render 404 - this gets hit every time
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        if (!req.body.pic) {
-            req.body.pic = '/public/images/default.jpg'
-        }
-        if (!req.body.city) {
-            req.body.city = 'Somewhere'
-        }
-        if (!req.body.country) {
-            req.body.country = 'Someplace'
-        }
-
-        places[id] = req.body
-        res.redirect(`/places/${id}`)
-    }
+    res.send('PUT /places/:id stub')
 });
 
-
-// CREATE //
-router.post('/', (req, res) => {
-
-    if (!req.body.pic) {
-        req.body.pic = '/images/default.jpg'
-    }
-    if (!req.body.city) {
-        req.body.city = 'Someplace'
-    }
-    if (!req.body.country) {
-        req.body.country = 'Somewhere'
-    }
-    places.push(req.body);
-    res.redirect('/places') // it being literal is almost confusing //
-});
-
-// DELETE //
+// DELETE // 
 router.delete('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        places.splice(id, 1)
-        res.redirect('/places')
-    }
+    res.send('DELETE /places/:id stub')
+});
+
+// EDIT // 
+router.get('/:id/edit', (req, res) => {
+    res.send('GET edit form stub')
+});
+
+// POST RANT //
+router.post('/:id/rant', (req, res) => {
+    res.send('GET /places/:id/rant stub')
+});
+
+// DELETE RANT //
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+// const router = require('express').Router();
+// const places = require('../models/places.js');
+
+// // HOME-INDEX //
+// router.get('/', (req, res) => {
+
+//     res.render('places/index', { places })
+// });
+
+// // NEW //
+// router.get('/new', (req, res) => {
+//     res.render('places/new')
+// });
+
+// // SHOW //
+// router.get('/:id', (req, res) => {
+//     let id = Number(req.params.id)
+//     if (isNaN(id)) {
+//         res.render('error404')
+//     }
+//     else if (!places[id]) {
+//         res.render('error404')
+//     }
+//     else {
+//         res.render('places/show', { place: places[id], id: id })
+//     }
+// });
+
+// // EDIT // PASSED CHECK
+// router.get('/:id/edit', (req, res) => {
+//     let id = Number(req.params.id)
+//     if (isNaN(id)) {
+//         res.render('error404')
+//     }
+//     else if (!places[id]) {
+//         res.render('error404')
+//     }
+//     else {
+//         res.render('places/edit', { place: places[id], id: id })
+//     }
+// });
+
+// // UPDATE //
+// router.put('/:id', (req, res) => {
+//     let id = Number(req.params.id);
+//     console.log('id:', id);
+//     console.log('places:', places);
+//     if (isNaN(id)) {
+//         res.render('error404')
+//     }
+//     else if (!places[id]) {
+//         res.render('error404')
+//     }
+//     else {
+//         if (!req.body.pic) {
+//             req.body.pic = '/public/images/default.jpg'
+//         }
+//         if (!req.body.city) {
+//             req.body.city = 'Somewhere'
+//         }
+//         if (!req.body.country) {
+//             req.body.country = 'Someplace'
+//         }
+
+//         places[id] = req.body
+//         res.redirect(`/places/${id}`)
+//     }
+// });
+
+
+// // CREATE //
+// router.post('/', (req, res) => {
+
+//     if (!req.body.pic) {
+//         req.body.pic = '/images/default.jpg'
+//     }
+//     if (!req.body.city) {
+//         req.body.city = 'Someplace'
+//     }
+//     if (!req.body.country) {
+//         req.body.country = 'Somewhere'
+//     }
+//     places.push(req.body);
+//     res.redirect('/places') // it being literal is almost confusing //
+// });
+
+// // DELETE //
+// router.delete('/:id', (req, res) => {
+//     let id = Number(req.params.id)
+//     if (isNaN(id)) {
+//         res.render('error404')
+//     }
+//     else if (!places[id]) {
+//         res.render('error404')
+//     }
+//     else {
+//         places.splice(id, 1)
+//         res.redirect('/places')
+//     }
+// });
+
+// module.exports = router;
--- models/places.js
+++ models/places.js
@@ -1,15 +1,36 @@
-module.exports = [{
-    name: 'Grace O\'Malleys',
-    city: 'Toronto',
-    country: 'Canada',
-    cuisines: 'American, Irish',
-    pic: '/images/graceOMalleys.jpg',
-    // Photo by<a href = "https://unsplash.com/@suicide_chewbacca?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ashwini Chaudhary(Monty)</a> on < a href = "https://unsplash.com/photos/a-building-with-a-sign-that-says-grace-owalleys-restaurant-B1wo-FSnAYA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" > Unsplash</ >
+const mongoose = require('mongoose')
 
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'New York',
-    country: 'United States',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/catCafe.jpg'
-}];
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: String,
+    cuisines: { type: String, required: true },
+    city: { type: String, default: 'Somewhere' },
+    country: { type: String, default: 'Someplace' },
+    founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+
+
+
+
+
+
+
+
+// module.exports = [{
+//     name: 'Grace O\'Malleys',
+//     city: 'Toronto',
+//     country: 'Canada',
+//     cuisines: 'American, Irish',
+//     pic: '/images/graceOMalleys.jpg',
+//     // Photo by<a href = "https://unsplash.com/@suicide_chewbacca?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ashwini Chaudhary(Monty)</a> on < a href = "https://unsplash.com/photos/a-building-with-a-sign-that-says-grace-owalleys-restaurant-B1wo-FSnAYA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" > Unsplash</ >
+
+// }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'New York',
+//     country: 'United States',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/catCafe.jpg'
+// }];
