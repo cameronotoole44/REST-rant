da1be95cc3c8fb2ca0aa79899a0af585cb0cc029
cameronotoole44 <cameronotoole404@gmail.com>
2024-05-03 13:02:18 -0400

    validations

--- models/places.js
+++ models/places.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const placeSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    pic: String,
+    pic: { type: String, default: '/images/default.jpg' }, // default img not showing proper ? //
     cuisines: { type: String, required: true },
     city: { type: String, default: 'Somewhere' },
     country: { type: String, default: 'Someplace' },
-    founded: Number
+    founded: {
+        type: Number, min: [600, 'THAT OLD HUH?!'],
+        max: [new Date().getFullYear(), 'SO YOU\'RE FROM THE FUTURE HUH?']
+    }
 });
 
 placeSchema.methods.showEstablished = function () {
